8578d5fadb3cbd9c3bd885a1ae9b17cfda1f2b41
joeldelacalle <joeldelacalle@gmail.com>
2021-04-01 10:20:59 +0200

    Añadido JDO al pom.xml, carpeta resources y adaptación de la clase App para funcionamiento de JDO

--- src/main/java/es/deusto/spq/App.java
+++ src/main/java/es/deusto/spq/App.java
@@ -1,9 +1,15 @@
 package es.deusto.spq;
 
+import javax.jdo.annotations.Inheritance;
+import javax.jdo.annotations.InheritanceStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+
 /**
  * Hello world!
  *
  */
+@PersistenceCapable
+@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
 public class App 
 {
     public static void main( String[] args )
