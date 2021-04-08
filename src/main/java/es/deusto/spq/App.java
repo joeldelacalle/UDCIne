2afc1b2c3f2e1515516b55bdba801d5265d74a6a
JonIbarreche <55731819+JonIbarreche@users.noreply.github.com>
2021-04-08 17:59:45 +0200

    Venta de Inicio creada
    Clas VentanaLoginCine creada y tambien inicializado en App.java

--- src/main/java/es/deusto/spq/App.java
+++ src/main/java/es/deusto/spq/App.java
@@ -4,16 +4,25 @@ import javax.jdo.annotations.Inheritance;
 import javax.jdo.annotations.InheritanceStrategy;
 import javax.jdo.annotations.PersistenceCapable;
 
+import java.awt.EventQueue;
+
 /**
  * Hello world!
  *
  */
 @PersistenceCapable
-@Inheritance(strategy=InheritanceStrategy.NEW_TABLE)
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@Inheritance(strategy = InheritanceStrategy.NEW_TABLE)
+public class App {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    VentanaLoginCine frame = new VentanaLoginCine();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
