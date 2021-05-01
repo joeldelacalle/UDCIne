dad23dc0e104b223179fbd2cc349e2c4b91209da
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-05-01 20:29:25 +0200

    test UsuarioResource

--- src/test/java/es/deusto/spq/jdo/UsuarioResourceTest.java
+++ src/test/java/es/deusto/spq/jdo/UsuarioResourceTest.java
@@ -2,15 +2,22 @@ package es.deusto.spq.jdo;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import es.deusto.spq.Cinema;
 import es.deusto.spq.Main;
+import es.deusto.spq.User;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
 
 public class UsuarioResourceTest {
 	
@@ -34,7 +41,16 @@ public class UsuarioResourceTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		WebTarget usersTarget = appTarget.path("users");
+    	
+	    List<User> listausers = Arrays.asList(new User("Jaime", "jaimesanta", "jaimesantamazo@opendeusto.es", "jaimesanta", 435345),new User("Jaime", "jaimesanta", "jaimesantamazo@opendeusto.es", "jaimesanta", 435345),new User("Jaime", "jaimesanta", "jaimesantamazo@opendeusto.es", "jaimesanta", 435345));
+
+	    GenericType<List<User>> genericType = new GenericType<List<User>>() {};
+	    List<User> users = usersTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+	    	
+	    assertEquals(listausers.get(0).getName(), users.get(0).getName());
+	    assertEquals(listausers.get(1).getName(), users.get(1).getName());
+	    assertEquals(listausers.get(2).getName(), users.get(2).getName());
 	}
 
 }
