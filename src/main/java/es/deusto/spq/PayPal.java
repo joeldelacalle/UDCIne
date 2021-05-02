5088c32496e8978902365eba1f78ee534f6b8952
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-05-02 22:39:03 +0200

    cambio clase paypal

--- src/main/java/es/deusto/spq/PayPal.java
+++ src/main/java/es/deusto/spq/PayPal.java
@@ -1,35 +1,14 @@
 package es.deusto.spq;
 
-import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.PersistenceCapable;
-import javax.jdo.annotations.Persistent;
-import javax.jdo.annotations.PrimaryKey;
+
 
 @PersistenceCapable
 public class PayPal {
 	
-	@PrimaryKey
-	@Persistent(valueStrategy = IdGeneratorStrategy.NATIVE)
-	protected long id;
-	
 	protected String email;
 	
 	protected String password;
-
-	public PayPal(long id, String email, String password) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.password = password;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
 
 	public String getEmail() {
 		return email;
@@ -46,10 +25,19 @@ public class PayPal {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public PayPal(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+
 	@Override
 	public String toString() {
-		return "PayPal [id=" + id + ", email=" + email + ", password=" + password + "]";
+		return "PayPal [email=" + email + ", password=" + password + "]";
 	}
+
+	
 
 
 }
