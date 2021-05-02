ef322e12af9044d6c1d8e9bd0bd6d33c570a9082
lucasdeprit <33033880+lucasdeprit@users.noreply.github.com>
2021-05-02 11:57:19 +0200

    CineResourcesTest con mockito

--- src/test/java/es/deusto/spq/jdo/CinemaResourceTest.java
+++ src/test/java/es/deusto/spq/jdo/CinemaResourceTest.java
@@ -1,6 +1,8 @@
 package es.deusto.spq.jdo;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +12,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.stubbing.Answer;
 
 import es.deusto.spq.Cinema;
 import es.deusto.spq.Main;
@@ -19,38 +22,33 @@ import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 
+
 public class CinemaResourceTest {
-	
-	private HttpServer server;
-    private WebTarget appTarget;
-    private Client c;
 
-    
-    @Before
-    public void setUp() throws Exception {
-    	server = Main.startServer();
-        c = ClientBuilder.newClient();
-        appTarget = c.target(Main.BASE_URI);
-    }
-    
-    @SuppressWarnings("deprecation")
-	@After
-    public void tearDown() throws Exception {
-        server.stop();
-    }
-    
+	CinemaResource cr = mock(CinemaResource.class);
+	List<Cinema> listacinesResources;
+
+	@Before
+	public void setUp() throws Exception {
+		List<Cinema> listacines = Arrays.asList(
+				new Cinema("Cine Deusto Zubiarte", "Bilbao", "Centro Comercial Zubiarte", 123456789),
+				new Cinema("Cine Deusto Santander", "Santander", "Corte Ingles nueva montaña", 345345345),
+				new Cinema("Cine Deusto Bakacaldo", "bakacaldo", "Max Center", 458345345));
+		when(cr.getReleases()).thenReturn(listacines);
+		listacinesResources = cr.getReleases();
+	}
+
 	@Test
-	public void test() {
-	    WebTarget cinemasTarget = appTarget.path("cinemas");
-	    	
-	    List<Cinema> listacines = Arrays.asList(new Cinema("Cine Deusto Zubiarte", "Bilbao", "Centro Comercial Zubiarte", 123456789),new Cinema("Cine Deusto Santander", "Santander", "Corte Ingles nueva montaña", 345345345),new Cinema("Cine Deusto Bakacaldo", "bakacaldo", "Max Center", 458345345));
+	public void testGetReleases() {
 
-	    GenericType<List<Cinema>> genericType = new GenericType<List<Cinema>>() {};
-	    List<Cinema> cines = cinemasTarget.request(MediaType.APPLICATION_JSON).get(genericType);
-	    	
-	    assertEquals(listacines.get(0).getName(), cines.get(0).getName());
-	    assertEquals(listacines.get(1).getName(), cines.get(1).getName());
-	    assertEquals(listacines.get(2).getName(), cines.get(2).getName());
+		List<Cinema> listacines = Arrays.asList(
+				new Cinema("Cine Deusto Zubiarte", "Bilbao", "Centro Comercial Zubiarte", 123456789),
+				new Cinema("Cine Deusto Santander", "Santander", "Corte Ingles nueva montaña", 345345345),
+				new Cinema("Cine Deusto Bakacaldo", "bakacaldo", "Max Center", 458345345));
+
+		assertEquals(listacines.get(0).getId(), cr.getReleases().get(0).getId());
+
 	}
 
 }
+
