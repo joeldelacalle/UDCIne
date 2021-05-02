4c535572b0d03a8c90f91f856e6651e22091243f
joeldelacalle <joeldelacalle@gmail.com>
2021-05-02 18:39:14 +0200

    modificado database.sql

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,3 +1,5 @@
+DROP SCHEMA IF EXISTS cine;
+DROP USER IF EXISTS 'spq'@'localhost';
 CREATE DATABASE cine;
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
 GRANT ALL ON cine.* TO 'spq'@'localhost';
