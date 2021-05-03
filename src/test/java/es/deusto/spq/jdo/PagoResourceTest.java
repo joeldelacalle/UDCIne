425b0c5abdb8bb020358538d97685d7ea0e484db
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-05-03 16:21:52 +0200

    test de pagoresource cambiado

--- src/test/java/es/deusto/spq/jdo/PagoResourceTest.java
+++ src/test/java/es/deusto/spq/jdo/PagoResourceTest.java
@@ -5,21 +5,25 @@ import static org.junit.Assert.*;
 import java.util.Arrays;
 import java.util.List;
 
+import org.databene.contiperf.PerfTest;
 import org.databene.contiperf.junit.ContiPerfRule;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import es.deusto.spq.Main;
 import es.deusto.spq.PayPal;
+import es.deusto.spq.types.IntegrationTest;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.MediaType;
 
+@Category(IntegrationTest.class)
 public class PagoResourceTest {
 
 	@Rule public ContiPerfRule rule = new ContiPerfRule();
@@ -41,13 +45,14 @@ public class PagoResourceTest {
         server.stop();
     }
     @Test
+    @PerfTest(invocations = 100, threads = 40)
     public void testPaypal() {
     	WebTarget pagosTarget = appTarget.path("paypal");
-    	WebTarget pagosPaypaliTarget = pagosTarget.path("getemail").queryParam("email", "jaimesantamazo@hotmail.com");
+    	WebTarget paypalTarget = pagosTarget.path("getemail").queryParam("email", "jaimesantamazo@hotmail.com");
     	List<PayPal> listapaypal = Arrays.asList(new PayPal("jaimesantamazo@hotmail.com", "123"));
     	 
     	GenericType<PayPal> genericType = new GenericType<PayPal>() {};
-		PayPal paypal = pagosPaypaliTarget.request(MediaType.APPLICATION_JSON).get(genericType);
+		PayPal paypal = paypalTarget.request(MediaType.APPLICATION_JSON).get(genericType);
 		 
 		assertEquals(listapaypal.get(0).getEmail(), paypal.getEmail());
     }
