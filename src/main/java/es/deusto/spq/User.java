05f9b35b7507dabb9eaa53c3c42678d779a732f2
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-05-06 18:49:27 +0200

    cambiado el userresource para hacer el login

--- src/main/java/es/deusto/spq/User.java
+++ src/main/java/es/deusto/spq/User.java
@@ -26,6 +26,7 @@ public class User {
 		this.password = password;
 		this.phoneNumber = phoneNumber;
 	}
+	public User() {}
 	
 	public long getId() {
 		return id;
--- src/main/java/es/deusto/spq/jdo/UserResource.java
+++ src/main/java/es/deusto/spq/jdo/UserResource.java
@@ -7,15 +7,18 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 
+import es.deusto.spq.PayPal;
 import es.deusto.spq.User;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("users")
 public class UserResource {
 	@GET
+	@Path("allusers")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<User> getUsers() {
 		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
@@ -29,6 +32,25 @@ public class UserResource {
 		pm.close();
 
 		return users;
+	}
+	@GET
+	@Path("getuser")
+	@Produces(MediaType.APPLICATION_JSON)
+	public static User getUser(@QueryParam("nickname") String nickname) {
+	PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("datanucleus.properties");
+	PersistenceManager pm = pmf.getPersistenceManager();
+
+	User user = new User();
+		
+	try (Query<User> u = pm.newQuery("SELECT FROM " + User.class.getName() + " WHERE nickname== '" + nickname + "'")) {
+		List<User> userlista = u.executeList();
+			
+		user = userlista.get(0);
+	} catch (Exception e) {
+		e.printStackTrace();
+	}
+	pm.close();
+	return user;
 	}
 
 }
