a6a6f90f41cf826e881bc29bddeee87eaf6a1ad6
lucasdeprit <33033880+lucasdeprit@users.noreply.github.com>
2021-05-18 20:12:09 +0200

    OrderWindowTest 89%

--- src/test/java/es/deusto/spq/gui/OrderWindowTest.java
+++ src/test/java/es/deusto/spq/gui/OrderWindowTest.java
@@ -2,6 +2,7 @@ package es.deusto.spq.gui;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.DefaultListModel;
@@ -10,6 +11,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import es.deusto.spq.Film;
+import es.deusto.spq.Order;
 import es.deusto.spq.Product;
 import es.deusto.spq.User;
 
@@ -59,6 +61,29 @@ public class OrderWindowTest {
 		ow.setProducts(products);
 
 		ow.CrearPedido(listModelShoppingCart, f, nickName);
+		
+		
+	
+		
+		OrderWindow ow2 = new OrderWindow(f);
+		Product p1 = new Product("Palomitas Medianas", "500g", 4,
+				"https://cdns3-2.primor.eu/90833-thickbox/cubo-palomitas-grande.jpg");
+		
+		Product p2 = new Product("Palomitas Grandes", "1000g", 6,
+				"https://cdns3-2.primor.eu/90833-thickbox/cubo-palomitas-grande.jpg");
+		
+		Product p3 = new Product("Palomitas Grandes + Coca Cola", "Unas palomitas grandes y coca cola", 8,
+				"https://previews.123rf.com/images/imagestore/imagestore1606/imagestore160601787/58756143-palomitas-en-rect%C3%A1ngulo-con-el-color-en-la-copa-para-llevar-aislado-en-el-fondo-blanco.jpg");
+		
+		List<Product> products2 = new ArrayList<Product>();
+		products2.add(p1);
+		products2.add(p2);
+		products2.add(p3);
+		
+		ow2.setProducts(products2);
+		
+		ow2.CrearPedido(listModelShoppingCart, f , nickName);
+		
 
 	}
 
