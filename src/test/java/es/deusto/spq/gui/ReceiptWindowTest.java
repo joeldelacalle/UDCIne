36c3adcdba235bfc8c97b47866bce6cf7cef545c
jaimesantamazo <55731728+jaimesantamazo@users.noreply.github.com>
2021-05-19 18:42:44 +0200

    Test ReceiptWindow

---
+++ src/test/java/es/deusto/spq/gui/ReceiptWindowTest.java
@@ -0,0 +1,62 @@
+package es.deusto.spq.gui;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.Main;
+import es.deusto.spq.User;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+
+public class ReceiptWindowTest {
+	
+	private JTextField textFieldPhoneNumber = new JTextField();
+	private JTextField textFieldName = new JTextField();
+	private JTextField textFieldNickname = new JTextField();
+	private JTextField textFieldEmail = new JTextField();
+	private JPasswordField passwordField = new JPasswordField();
+	private int phone;
+	private User u;
+	private ReceiptWindow rw;
+	Client client = ClientBuilder.newClient();
+	private HttpServer server;
+
+	final WebTarget appTarget = client.target("http://localhost:8080/myapp");
+	final WebTarget receiptTarget = appTarget.path("receipts");
+	
+	@Before
+	public void setUp() throws Exception {
+		server = Main.startServer();
+		u = new User();
+		textFieldName = new JTextField("Jaime");
+		textFieldNickname = new JTextField("jaimesanta");
+		textFieldEmail = new JTextField("jaimesantamazo@hotmail.com");
+		passwordField = new JPasswordField("1234");
+		textFieldPhoneNumber = new JTextField("99");
+		rw = new ReceiptWindow();
+		phone = Integer.parseInt(textFieldPhoneNumber.getText().toString());
+		u.setName(textFieldName.getText());
+		u.setNickname(textFieldNickname.getText());
+		u.setEmail(textFieldEmail.getText());
+		u.setPassword(passwordField.getText());
+		u.setPhoneNumber(phone);
+	}
+	@Test
+	public void ReceiptTest() {
+		rw.SetUserName(u);
+		rw.SetEmail(u);
+	}
+	@After
+	public void tearDown() throws Exception {
+		server.stop();
+	}
+}
