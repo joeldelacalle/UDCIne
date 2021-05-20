4b857e30f07482ec819f9e298cd4bb468e3f202b
nicocerero <nicolas.cerero@opendeusto.es>
2021-05-20 21:38:12 +0200

    Arreglo VipWindowTest

--- src/test/java/es/deusto/spq/gui/VipWindowTest.java
+++ src/test/java/es/deusto/spq/gui/VipWindowTest.java
@@ -18,6 +18,9 @@ public class VipWindowTest {
 	private User user;
 	private List<Order> listExpected;
 	private OrderResource or;
+	private Order o1;
+	private Order o2;
+	private Order o3;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -26,6 +29,16 @@ public class VipWindowTest {
 		user = new User();
 		or = new OrderResource();
 		listExpected = new ArrayList<Order>();
+		
+		o1 = new Order("jaimesantamazo@opendeusto.es", null, 3, "Pendiente de pago", "Vacio",
+				"pelicula:Infinity war Entrada:1 fila:1 asiento:1 Entrada:2 fila:2 asiento:2 Entrada:3 fila:3 asiento:3",
+				24);
+		
+		o2 = new Order("jaimesantamazo@opendeusto.es", null, 1, "Pendiente de pago", "Vacio",
+				"pelicula:Infinity war Entrada:1 fila:1 asiento:1", 8);
+		
+		o3 = new Order("jaimesantamazo@opendeusto.es", null, 2, "Pendiente de pago", "Vacio",
+				"pelicula:Infinity war Entrada:1 fila:1 asiento:1 Entrada:2 fila:2 asiento:2", 16);
 		
 	}
 	
@@ -45,6 +58,12 @@ public class VipWindowTest {
 		String lista = oList.toString();
 		
 		listExpected = vw.SetOrderRecord(user.getNickname());
+		o1.setId(1);
+		listExpected.add(o1);
+		o2.setId(2);
+		listExpected.add(o2);
+		o3.setId(3);
+		listExpected.add(o3);
 		String listaEsperada = listExpected.toString();
 	
 		assertEquals(lista, listaEsperada);
