5cd0eabafca72ee122f0a37d77bd50dcfea9560e
lucasdeprit <33033880+lucasdeprit@users.noreply.github.com>
2021-05-20 17:42:36 +0200

    MainWindowTest

---
+++ src/test/java/es/deusto/spq/gui/MainWindowTest.java
@@ -0,0 +1,49 @@
+package es.deusto.spq.gui;
+
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.spq.User;
+
+public class MainWindowTest {
+
+	
+	private MainWindow mw;
+	private User u;
+	private String urlS;
+	private JButton jbutton;
+	
+	@Before
+	public void setUp() {
+		
+		mw = new MainWindow();
+		u = new User();
+		urlS = "https://pics.filmaffinity.com/iron_man-108960873-large.jpg";
+		jbutton = new JButton();
+		
+	}
+	@Test
+	public void testSetUserName() {
+		
+		mw.SetUserName(u);
+	}
+	
+	@Test
+	public void SetEmail() {
+	
+		mw.SetEmail(u);
+	}
+	
+	@Test
+	public void btnSetImageIcon() throws IOException {
+		
+			mw.btnSetImageIcon(urlS, jbutton);
+		
+		
+	}
+}
