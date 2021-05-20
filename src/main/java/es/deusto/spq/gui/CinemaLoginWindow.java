9c4f050a88877d43e27a4d32fd5a990c0c43df70
lucasdeprit <33033880+lucasdeprit@users.noreply.github.com>
2021-05-20 13:55:20 +0200

    CinemaLoginWindowTest

--- src/main/java/es/deusto/spq/gui/CinemaLoginWindow.java
+++ src/main/java/es/deusto/spq/gui/CinemaLoginWindow.java
@@ -22,8 +22,6 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
-
-
 import es.deusto.spq.jdo.UserResource;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
@@ -31,245 +29,248 @@ import jakarta.ws.rs.client.WebTarget;
 
 public class CinemaLoginWindow extends JFrame {
 
-    private static final long serialVersionUID = 1L;
-    Client client = ClientBuilder.newClient();
+	private static final long serialVersionUID = 1L;
+	Client client = ClientBuilder.newClient();
 
 	final WebTarget appTarget = client.target("http://localhost:8080/myapp");
 	final WebTarget FilmsTarget = appTarget.path("films");
-    private JPanel contentPane;
-    private JTextField txtUsername1;
-    private JPasswordField txtPassword1;
-    private JLabel lblLoginMessage1 = new JLabel("");
-    private JLabel lblGest ;
-    private JLabel lblUserName = new JLabel("");
-    
-    public final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-    public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                try {
-                	
-                    CinemaLoginWindow frame = new CinemaLoginWindow();
-                    frame.setVisible(true);
-                } catch (Exception e) {
-                	logger.log(Level.WARNING, "ERROR",e);
-                    //e.printStackTrace();
-                }
-            }
-        });
-    }
+	private JPanel contentPane;
+	private JTextField txtUsername1;
+	private JPasswordField txtPassword1;
+	private JLabel lblLoginMessage1 = new JLabel("");
+	private JLabel lblGest;
+	
 
-    
-    public CinemaLoginWindow() {
+	public final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
-        setUndecorated(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 600, 400);
-        contentPane = new JPanel();
-        
-        contentPane.setBackground(new Color(72, 209, 204));
-        contentPane.setBorder(new LineBorder(new Color(85, 107, 47), 2));
-        setContentPane(contentPane);
-        contentPane.setLayout(null);
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
 
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.WHITE);
-        panel.setBounds(173, 146, 250, 40);
-        contentPane.add(panel);
-        panel.setLayout(null);
+					CinemaLoginWindow frame = new CinemaLoginWindow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					logger.log(Level.WARNING, "ERROR", e);
+					// e.printStackTrace();
+				}
+			}
+		});
+	}
 
-        txtUsername1 = new JTextField();
-        txtUsername1.addFocusListener(new FocusAdapter() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (txtUsername1.getText().equals("Username")) {
-                    txtUsername1.setText("");
-                } else {
-                    txtUsername1.selectAll();
-                }
-            }
+	public CinemaLoginWindow() {
 
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (txtUsername1.getText().equals(""))
-                    txtUsername1.setText("Username");
-            }
-        });
-        txtUsername1.setBorder(null);
-        txtUsername1.setFont(new Font("Arial", Font.BOLD, 14));
-        txtUsername1.setText("Usuario");
-        txtUsername1.setBounds(10, 10, 170, 20);
-        panel.add(txtUsername1);
-        txtUsername1.setColumns(10);
-        //
+		setUndecorated(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 400);
+		contentPane = new JPanel();
 
-        JPanel lblIconPassword1 = new JPanel();
-        lblIconPassword1.setBackground(Color.WHITE);
-        lblIconPassword1.setBounds(173, 213, 250, 40);
-        contentPane.add(lblIconPassword1);
-        lblIconPassword1.setLayout(null);
+		contentPane.setBackground(new Color(72, 209, 204));
+		contentPane.setBorder(new LineBorder(new Color(85, 107, 47), 2));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
 
-        txtPassword1 = new JPasswordField();
-        txtPassword1.addFocusListener(new FocusAdapter() {
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (txtPassword1.getText().equals("Password")) {
-                    txtPassword1.setEchoChar('-');
-                    txtPassword1.setText("");
-                } else {
-                    txtPassword1.selectAll();
-                }
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(173, 146, 250, 40);
+		contentPane.add(panel);
+		panel.setLayout(null);
 
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                if (txtPassword1.getText().equals("")) {
-                    txtPassword1.setText("Password");
-                    txtPassword1.setEchoChar((char) 0);
-                }
-            }
-        });
-        txtPassword1.setBorder(null);
-        txtPassword1.setEchoChar((char) 0);
-        txtPassword1.setFont(new Font("Arial", Font.BOLD, 14));
-        txtPassword1.setText("Contraseña");
-        txtPassword1.setBounds(10, 10, 170, 20);
-        lblIconPassword1.add(txtPassword1);
-
-        final JPanel pnlBtnLogin1 = new JPanel();
-        pnlBtnLogin1.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                Login();
-            }
-
-			private void Login() {
-				System.out.println(txtPassword1.getPassword().toString());
-				UserResource ur = new UserResource();
-				if (txtUsername1.getText().equals("admin") && txtPassword1.getText().equals("admin")) {
-                    // los datos son correctos
-                    lblLoginMessage1.setText("");
-                    AdminWindow aw = new AdminWindow ();
-            		aw.setVisible(true);
-            		dispose();
-				}else if(ur.CheckUser(txtUsername1.getText(), txtPassword1.getText().toString())){
-					lblLoginMessage1.setText("te has loggeado correctamente!");
-					MainWindow vp = new MainWindow ();
-					vp.SetUserName(ur.getUser(txtUsername1.getText()));
-	        		vp.setVisible(true);
-	        		dispose();
-					
-                } else if (txtUsername1.getText().equals("") || txtUsername1.getText().equals("Username")
-                        || txtPassword1.getText().equals("") || txtPassword1.getText().equals("Password")) {
-                    lblLoginMessage1.setText("Por favor rellena los campos!");
-                } else  {
-                    lblLoginMessage1.setText("Usuario y contraseña no coinciden");
-                }
+		txtUsername1 = new JTextField();
+		txtUsername1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (txtUsername1.getText().equals("Username")) {
+					txtUsername1.setText("");
+				} else {
+					txtUsername1.selectAll();
+				}
 			}
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                pnlBtnLogin1.setBackground(new Color(20, 30, 40));
-            }
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (txtUsername1.getText().equals(""))
+					txtUsername1.setText("Username");
+			}
+		});
+		txtUsername1.setBorder(null);
+		txtUsername1.setFont(new Font("Arial", Font.BOLD, 14));
+		txtUsername1.setText("Usuario");
+		txtUsername1.setBounds(10, 10, 170, 20);
+		panel.add(txtUsername1);
+		txtUsername1.setColumns(10);
+		//
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                pnlBtnLogin1.setBackground(new Color(47, 79, 79));
-            }
+		JPanel lblIconPassword1 = new JPanel();
+		lblIconPassword1.setBackground(Color.WHITE);
+		lblIconPassword1.setBounds(173, 213, 250, 40);
+		contentPane.add(lblIconPassword1);
+		lblIconPassword1.setLayout(null);
 
-            @Override
-            public void mousePressed(MouseEvent e) {
-                pnlBtnLogin1.setBackground(new Color(60, 80, 90));
-            }
+		txtPassword1 = new JPasswordField();
+		txtPassword1.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (txtPassword1.getText().equals("Password")) {
+					txtPassword1.setEchoChar('-');
+					txtPassword1.setText("");
+				} else {
+					txtPassword1.selectAll();
+				}
 
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                pnlBtnLogin1.setBackground(new Color(20, 30, 40));
-            }
-        });
-        pnlBtnLogin1.setBackground(new Color(47, 79, 79));
-        pnlBtnLogin1.setBounds(327, 292, 198, 51);
-        contentPane.add(pnlBtnLogin1);
-        pnlBtnLogin1.setLayout(null);
+			}
 
-        final JLabel lblLogIn = new JLabel("INICIAR SESIÓN");
-        lblLogIn.setForeground(Color.WHITE);
-        lblLogIn.setFont(new Font("Arial", Font.BOLD, 14));
-        lblLogIn.setBounds(47, 10, 125, 31);
-        pnlBtnLogin1.add(lblLogIn);
+			@Override
+			public void focusLost(FocusEvent e) {
+				if (txtPassword1.getText().equals("")) {
+					txtPassword1.setText("Password");
+					txtPassword1.setEchoChar((char) 0);
+				}
+			}
+		});
+		txtPassword1.setBorder(null);
+		txtPassword1.setEchoChar((char) 0);
+		txtPassword1.setFont(new Font("Arial", Font.BOLD, 14));
+		txtPassword1.setText("Contraseña");
+		txtPassword1.setBounds(10, 10, 170, 20);
+		lblIconPassword1.add(txtPassword1);
 
-        final JLabel lblX = new JLabel("X");
-        lblX.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (JOptionPane.showConfirmDialog(null, "Seguro que quieres salir de la aplicacion?", "Confirmar",
-                        JOptionPane.YES_NO_OPTION) == 0) {
-                    CinemaLoginWindow.this.dispose();
-                }
-            }
+		final JPanel pnlBtnLogin1 = new JPanel();
+		pnlBtnLogin1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Login(txtPassword1, txtUsername1);
+			}
 
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                lblX.setForeground(Color.red);
-            }
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				pnlBtnLogin1.setBackground(new Color(20, 30, 40));
+			}
 
-            @Override
-            public void mouseExited(MouseEvent e) {
-                lblX.setForeground(Color.WHITE);
-            }
-        });
-        lblX.setForeground(Color.WHITE);
-        lblX.setFont(new Font("Arial", Font.BOLD, 25));
-        lblX.setHorizontalAlignment(SwingConstants.CENTER);
-        lblX.setBounds(563, 0, 37, 34);
-        contentPane.add(lblX);
+			@Override
+			public void mouseExited(MouseEvent e) {
+				pnlBtnLogin1.setBackground(new Color(47, 79, 79));
+			}
 
-        lblLoginMessage1.setForeground(new Color(128, 0, 0));
-        lblLoginMessage1.setFont(new Font("Arial", Font.PLAIN, 12));
-        lblLoginMessage1.setBounds(173, 263, 250, 19);
-        contentPane.add(lblLoginMessage1);
+			@Override
+			public void mousePressed(MouseEvent e) {
+				pnlBtnLogin1.setBackground(new Color(60, 80, 90));
+			}
 
-        JLabel lblCineDeusto = new JLabel("CINE DEUSTO");
-        lblCineDeusto.setFont(new Font("Cooper Black", Font.BOLD, 33));
-        lblCineDeusto.setToolTipText("");
-        lblCineDeusto.setBounds(175, 68, 299, 34);
-        contentPane.add(lblCineDeusto);
-        
-        lblGest = new JLabel("Entrar como invitado");
-        lblGest.addMouseListener(new MouseAdapter() {
-        	@Override
-        	public void mouseEntered(MouseEvent e) {
-        		 lblGest.setForeground(Color.RED);
-        	}
-        	@Override
-        	public void mouseExited(MouseEvent e) {
-        		lblGest.setForeground(Color.BLUE);
-        	}
-        	@Override
-        	public void mouseClicked(MouseEvent e) {
-        		MainWindow vp = new MainWindow ();
-        		vp.setVisible(true);
-        		dispose();
-        	}
-        });
-        lblGest.setForeground(Color.BLUE);
-        lblGest.setBounds(237, 362, 151, 14);
-        contentPane.add(lblGest);
-        
-        JButton btnRegister = new JButton("REGISTRARSE");
-        btnRegister.addActionListener(new ActionListener() {
-        	public void actionPerformed(ActionEvent e) {
-        		RegisterWindow rw = new RegisterWindow();
-        		rw.setVisible(true);
-        		dispose();
-        	}
-        });
-        btnRegister.setFont(new Font("Tahoma", Font.BOLD, 15));
-        btnRegister.setForeground(Color.WHITE);
-        btnRegister.setBackground(new Color(47, 79, 79));
-        btnRegister.setBounds(59, 292, 176, 51);
-        contentPane.add(btnRegister);
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				pnlBtnLogin1.setBackground(new Color(20, 30, 40));
+			}
+		});
+		pnlBtnLogin1.setBackground(new Color(47, 79, 79));
+		pnlBtnLogin1.setBounds(327, 292, 198, 51);
+		contentPane.add(pnlBtnLogin1);
+		pnlBtnLogin1.setLayout(null);
 
-    }
+		final JLabel lblLogIn = new JLabel("INICIAR SESIÓN");
+		lblLogIn.setForeground(Color.WHITE);
+		lblLogIn.setFont(new Font("Arial", Font.BOLD, 14));
+		lblLogIn.setBounds(47, 10, 125, 31);
+		pnlBtnLogin1.add(lblLogIn);
+
+		final JLabel lblX = new JLabel("X");
+		lblX.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (JOptionPane.showConfirmDialog(null, "Seguro que quieres salir de la aplicacion?", "Confirmar",
+						JOptionPane.YES_NO_OPTION) == 0) {
+					CinemaLoginWindow.this.dispose();
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblX.setForeground(Color.red);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblX.setForeground(Color.WHITE);
+			}
+		});
+		lblX.setForeground(Color.WHITE);
+		lblX.setFont(new Font("Arial", Font.BOLD, 25));
+		lblX.setHorizontalAlignment(SwingConstants.CENTER);
+		lblX.setBounds(563, 0, 37, 34);
+		contentPane.add(lblX);
+
+		lblLoginMessage1.setForeground(new Color(128, 0, 0));
+		lblLoginMessage1.setFont(new Font("Arial", Font.PLAIN, 12));
+		lblLoginMessage1.setBounds(173, 263, 250, 19);
+		contentPane.add(lblLoginMessage1);
+
+		JLabel lblCineDeusto = new JLabel("CINE DEUSTO");
+		lblCineDeusto.setFont(new Font("Cooper Black", Font.BOLD, 33));
+		lblCineDeusto.setToolTipText("");
+		lblCineDeusto.setBounds(175, 68, 299, 34);
+		contentPane.add(lblCineDeusto);
+
+		lblGest = new JLabel("Entrar como invitado");
+		lblGest.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				lblGest.setForeground(Color.RED);
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				lblGest.setForeground(Color.BLUE);
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				MainWindow vp = new MainWindow();
+				vp.setVisible(true);
+				dispose();
+			}
+		});
+		lblGest.setForeground(Color.BLUE);
+		lblGest.setBounds(237, 362, 151, 14);
+		contentPane.add(lblGest);
+
+		JButton btnRegister = new JButton("REGISTRARSE");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				RegisterWindow rw = new RegisterWindow();
+				rw.setVisible(true);
+				dispose();
+			}
+		});
+		btnRegister.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnRegister.setForeground(Color.WHITE);
+		btnRegister.setBackground(new Color(47, 79, 79));
+		btnRegister.setBounds(59, 292, 176, 51);
+		contentPane.add(btnRegister);
+
+	}
+
+	public void Login(JPasswordField txtPassword1, JTextField txtUsername1) {
+		System.out.println(txtPassword1.getPassword().toString());
+		UserResource ur = new UserResource();
+		if (txtUsername1.getText().equals("admin") && txtPassword1.getText().equals("admin")) {
+			// los datos son correctos
+			lblLoginMessage1.setText("");
+			AdminWindow aw = new AdminWindow();
+			aw.setVisible(true);
+			dispose();
+		} else if (ur.CheckUser(txtUsername1.getText(), txtPassword1.getText().toString())) {
+			lblLoginMessage1.setText("te has loggeado correctamente!");
+			MainWindow vp = new MainWindow();
+			vp.SetUserName(ur.getUser(txtUsername1.getText()));
+			vp.setVisible(true);
+			dispose();
+
+		} else if (txtUsername1.getText().equals("") || txtUsername1.getText().equals("Username")
+				|| txtPassword1.getText().equals("") || txtPassword1.getText().equals("Password")) {
+			lblLoginMessage1.setText("Por favor rellena los campos!");
+		} else {
+			lblLoginMessage1.setText("Usuario y contraseña no coinciden");
+		}
+	}
+
 }
---
+++ src/test/java/es/deusto/spq/gui/CinemaLoginWindowTest.java
@@ -0,0 +1,55 @@
+package es.deusto.spq.gui;
+
+import static org.junit.Assert.*;
+
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CinemaLoginWindowTest {
+
+	private JTextField txtUsername1;
+	private JPasswordField txtPassword1;
+	
+	private JTextField txtUsername2;
+	private JPasswordField txtPassword2;
+	private JTextField txtUsername3;
+	private JPasswordField txtPassword3;
+	
+	private CinemaLoginWindow lw;
+	private CinemaLoginWindow lw2;
+	private CinemaLoginWindow lw3;
+	@Before
+	public void setUp() {
+		txtUsername1 = new JTextField();
+		txtPassword1 = new JPasswordField();
+		
+		txtUsername2 = new JTextField();
+		txtPassword2 = new JPasswordField();
+		
+		txtUsername3 = new JTextField();
+		txtPassword3 = new JPasswordField();
+		
+		lw = new CinemaLoginWindow();
+		lw2 = new CinemaLoginWindow();
+		lw3 = new CinemaLoginWindow();
+	}
+	@Test
+	public void testLogin() {
+		txtUsername1.setText("jaimesanta");
+		txtPassword1.setText("jaimesanta");
+		
+		txtUsername2.setText("admin");
+		txtPassword2.setText("admin");
+		
+		txtUsername3.setText("");
+		txtPassword3.setText("");
+		
+		lw.Login(txtPassword1, txtUsername1);
+		lw2.Login(txtPassword2, txtUsername2);
+		lw3.Login(txtPassword3, txtUsername3);
+	}
+
+}
