3aa54fe3a4948da6a2974e2e2e77bbe72850e0a0
nicocerero <nicolas.cerero@opendeusto.es>
2021-05-21 11:53:38 +0200

    Métodos de FilmWindow a public y FilmWindowTest terminado

--- src/test/java/es/deusto/spq/gui/FilmWindowTest.java
+++ src/test/java/es/deusto/spq/gui/FilmWindowTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.*;
 import java.awt.Image;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.imageio.ImageIO;
 import javax.swing.DefaultListModel;
@@ -17,161 +21,101 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import es.deusto.spq.Billboard;
 import es.deusto.spq.Film;
 import es.deusto.spq.Main;
 import es.deusto.spq.Release;
+import es.deusto.spq.User;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 
 public class FilmWindowTest {
-	
+
 	private URL url;
-	
-	private JLabel lblRecommendedAge = new JLabel("");
-	private JLabel textFieldFilmName = new JLabel("");
-	private JLabel lblFilmImage = new JLabel("");
-	private JLabel textPaneDescription = new JLabel("");
-	
+
 	private Film f;
+	private FilmWindow fw;
+	private int selectedFilm;
+
+	private JComboBox<String> comboBox;
+
+	private User u;
 	
+	private List<Billboard> billList;
+	private List<Film> filmList; 
+
 	private HttpServer server;
 	private WebTarget appTarget;
-	private WebTarget filmTarget;
-	
-	
-	private Image image;
-	private String descFilm;
-	private int ageAll; // apta para todos los publicos
-	private int age7;
-	private int age13;
-	private int age16;
+
+	public final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
 	@Before
 	public void setUp() throws Exception {
-		
-		ageAll = 0;
-		age7 = 7;
-		age13 = 13;
-		age16 = 16;
-		
-		lblRecommendedAge = new JLabel();
-		f = new Film("Jon", "Infinity war",
-					"El todopoderoso Thanos ha despertado con la promesa de arrasar con todo a su paso, portando el Guantelete del Infinito. Los únicos capaces de pararle los pies son los Vengadores y el resto de superhéroes de la galaxia",
-					13, "https://pics.filmaffinity.com/avengers_infinity_war-181539353-large.jpg", "https://www.youtube.com/watch?v=6ZfuNTqbHE8");
-		
-		url = new URL(f.getUrl());
-		
+
 		server = Main.startServer();
 		Client c = ClientBuilder.newClient();
 
 		appTarget = c.target(Main.BASE_URI);
-		filmTarget = appTarget.path("films");
+
+		u = new User();
+
+		f = new Film("Jon", "Infinity war",
+				"El todopoderoso Thanos ha despertado con la promesa de arrasar con todo a su paso, portando el Guantelete del Infinito. Los únicos capaces de pararle los pies son los Vengadores y el resto de superhéroes de la galaxia",
+				13, "https://pics.filmaffinity.com/avengers_infinity_war-181539353-large.jpg",
+				"https://www.youtube.com/watch?v=6ZfuNTqbHE8");
+
+		comboBox = new JComboBox<String>();
+		comboBox.addItem(f.toString());
+		comboBox.setSelectedItem(f.toString());
+
+		url = new URL(f.getUrl());
+
+		selectedFilm = 0;
+		
+		billList = new ArrayList<Billboard>();
+		filmList = new ArrayList<Film>();
+		
+		fw = new FilmWindow(selectedFilm);
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 		server.stop();
 	}
 
-
 	@Test
-	public void ageFilmIconResize(){
-		
+	public void ageFilmIconResizeTest() {
+
 		try {
-			image = ImageIO.read(url);
+			fw.ageFilmIconResize(url);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.log(Level.WARNING, "ERROR", e);
 		}
-		ImageIcon myImg = new ImageIcon(url);
-		image = myImg.getImage();
+	}
 
-		int width = myImg.getIconWidth() / 5;
-		//System.out.println(width);
-		int height = myImg.getIconHeight() / 5;
-		//System.out.println(height);
+	@Test
+	public void nuevasPeliculasTest() {
 
-		Image newImg = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-		ImageIcon resizeImg = new ImageIcon(newImg);
-		lblRecommendedAge.setIcon(resizeImg);
-		assertEquals(resizeImg, lblRecommendedAge.getIcon());
-		//assertEquals(resizeImg.getIconWidth(), width);
+		fw.nuevasPeliculas(comboBox);
+
+	}
+
+	@Test
+	public void filmAgeRestImageTest() {
+
+		fw.filmAgeRestImage();
+
+	}
+
+	@Test
+	public void SetUserNameTest() {
+		fw.SetUserName(u);
 	}
 	
 	@Test
-	public void nuevasPeliculas() {
-		
-		
-		textFieldFilmName.setText(f.getName());
-		String selectedFilm = textFieldFilmName.getText();
-		final DefaultListModel<Film> films = new DefaultListModel<>();
-		films.addElement(new Film("Jon", "Infinity war",
-				"El todopoderoso Thanos ha despertado con la promesa de arrasar con todo a su paso, portando el Guantelete del Infinito. Los únicos capaces de pararle los pies son los Vengadores y el resto de superhéroes de la galaxia",
-				13, "https://pics.filmaffinity.com/avengers_infinity_war-181539353-large.jpg", "https://www.youtube.com/watch?v=6ZfuNTqbHE8"));
-		assertEquals(films.get(0).getName(), selectedFilm);
-		
-		if (films.get(0).getName().equals(selectedFilm)) {
-			try {
-				descFilm=films.get(0).getDescription();
-				url = new URL(films.get(0).getUrl());
-				image = ImageIO.read(url);
-				ImageIcon myImg = new ImageIcon(url);
-				image = myImg.getImage();
-
-				int width = myImg.getIconWidth() / 7 * 2;
-				// System.out.println(width);
-				int height = myImg.getIconHeight() / 7 * 2;
-				// System.out.println(height);
-
-				Image newImg = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-				ImageIcon resizeImg = new ImageIcon(newImg);
-				
-				lblFilmImage.setIcon(resizeImg);
-				assertEquals(lblFilmImage.getIcon(), resizeImg);
-				textPaneDescription.setText(descFilm);
-				assertEquals(films.get(0).getDescription(), descFilm);
-			} catch (IOException e7) {
-			}
-		}
-	
+	public void transformFilmsTest() {
+		fw.transformFilms(billList, filmList);
 	}
-	@Test
-	public void filmAgeRestImage() {
-		if (ageAll == f.getAgeRestriction()) {
-			try {
-				url = new URL("https://upload.wikimedia.org/wikipedia/commons/6/68/Edad_TP.png");
-				assertEquals("https://upload.wikimedia.org/wikipedia/commons/6/68/Edad_TP.png", url.toString());
-			} catch (IOException e7) {
-			}
-		} else if (age7 == f.getAgeRestriction()) {
-			try {
-				url = new URL("https://upload.wikimedia.org/wikipedia/commons/5/55/Edad_7.png");
-				assertEquals("https://upload.wikimedia.org/wikipedia/commons/5/55/Edad_7.png", url.toString());
-			} catch (IOException e7) {
-			}
-
-		} else if (age13 == f.getAgeRestriction()) {
-			try {
-				url = new URL("https://upload.wikimedia.org/wikipedia/commons/b/bd/Edad_13.png");
-				assertEquals("https://upload.wikimedia.org/wikipedia/commons/b/bd/Edad_13.png", url.toString());
-			} catch (IOException e7) {
-			}
-
-		} else if (age16 == f.getAgeRestriction()) {
-			try {
-				url = new URL("https://upload.wikimedia.org/wikipedia/commons/4/4b/Edad_16.png");
-				assertEquals("https://upload.wikimedia.org/wikipedia/commons/4/4b/Edad_16.png", url.toString());
-			} catch (IOException e7) {
-			}
-
-		} else {
-			try {
-				url = new URL("https://upload.wikimedia.org/wikipedia/commons/c/ca/Edad_18.png");
-				assertEquals("https://upload.wikimedia.org/wikipedia/commons/c/ca/Edad_18.png", url.toString());
-			} catch (IOException e7) {
-			}
-		}
-	}
-
 
 }
